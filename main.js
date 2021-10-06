2837b4216028dfe744737cf01c4e46351210bd4a
Techie Coder <71074789+techiecoder2079@users.noreply.github.com>
2021-10-06 10:19:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -179,6 +179,32 @@
 
     // ***********nice select for dropdown 
     $('select').niceSelect();
+    
+    // **************** num *******************
+    $('.num-in span').click(function () {
+        var $input = $(this).parents('.num-block').find('input.in-num');
+        if ($(this).hasClass('minus')) {
+            var count = parseFloat($input.val()) - 1;
+            count = count < 1 ? 1 : count;
+            if (count < 2) {
+                $(this).addClass('dis');
+            }
+            else {
+                $(this).removeClass('dis');
+            }
+            $input.val(count);
+        }
+        else {
+            var count = parseFloat($input.val()) + 1;
+            $input.val(count);
+            if (count > 1) {
+                $(this).parents('.num-block').find(('.minus')).removeClass('dis');
+            }
+        }
+        $input.change();
+        return false;
+
+    });
 
 
 
